e86067109cf1b28f99593cbc57be1d27c339a029
jemikco <jemikcolomboy12910@gmail.com>
2026-09-18 10:42:40 +0800

    "Added Calvin's Test Profile"

---
+++ app/src/test/java/quarter2/DejesusProfile.java
@@ -0,0 +1,18 @@
+package quarter2;
+
+import org.junit.Test;
+public class DejesusProfile {
+    @Test
+    public void printMyProfile() {
+
+        String myName = "Calvin De Jesus";
+        String petName = "Peter";
+        String favFood = "Sinigang";
+        int myAge = 17;
+
+        System.out.println("--- MY DIGITAL PROFILE ---");
+        System.out.println("Hello, my name is" + myName + "and I am" + myAge + "years old");
+        System.out.println("I have a cutie pet named" + petName + ".");
+        System.out.println("If I could, I would" + favFood + "every single day!");
+    }
+}
